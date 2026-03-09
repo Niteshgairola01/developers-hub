15ee125ebf69487eaf2547420ea6dd8d0da9a2ae
Niteshgairola01 <niteshgairolapapl@gmail.com>
2026-03-09 11:54:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen min-h-screen'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, ex.
+      </div>
+    </>
+  )
+}
+
+export default App
